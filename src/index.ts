8df0456b4530fb31a1fee42f8ace299e9fdb4c8c
Badass-Nemesis <harshitanant29@gmail.com>
2025-01-03 12:47:31 +0530

    plan changed, using js only now

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
-
-export const handler: APIGatewayProxyHandler = async (event) => {
-    console.log("hello world");
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" }),
-    }
-}
